d6bff41a6815893083d67be0d10eb9a39780d6d1
ShinHyungJune <ShinHyungJune>
2024-03-20 08:45:34 +0900

    fix

--- pages/investgator/campaigns/index.vue
+++ pages/investgator/campaigns/index.vue
@@ -265,7 +265,7 @@ export default {
 
             this.$store.commit("setLoading", true);
 
-            this.form.delete("/api/investgator/investgators")
+            this.usersForm.delete("/api/investgator/investgators")
                     .then(response => {
                         this.getUsers();
                     });
@@ -277,7 +277,7 @@ export default {
 
             this.$store.commit("setLoading", true);
 
-            this.form.post("/api/investgator/investgators")
+            this.usersForm.post("/api/investgator/investgators")
                     .then(response => {
                         this.getUsers();
                     });
