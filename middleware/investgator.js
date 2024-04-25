0307223c61135cbfb58c13b9fbf9814f7e216000
ShinHyungJune <ssa4141@naver.com>
2024-04-26 02:00:35 +0900

    에러문구 스타일 조정

--- middleware/investgator.js
+++ middleware/investgator.js
@@ -1,5 +1,5 @@
 export default function ({store,route, redirect, next}) {
-    if(!store.state.auth.user || store.state.auth.user.data.type !== 'AGENCY_ADMIN') {
+    if(!store.state.auth.user || (store.state.auth.user.data.type !== 'AGENCY_ADMIN' && store.state.auth.user.data.type !== 'ADMIN')) {
         redirect("/admin/login");
 
         store.commit("setIntendedUrl", route.fullPath);
