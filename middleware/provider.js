51f92f32426e703b0c820b4d85ba5162b8916787
ShinHyungJune <ssa4141@naver.com>
2024-06-20 17:09:29 +0900

    fix

--- middleware/provider.js
+++ middleware/provider.js
@@ -1,6 +1,6 @@
 export default function ({store,route, redirect, next}) {
 
-    if(!store.state.auth.user || (store.state.auth.user.data && !store.state.auth.user.data.provider)) {
+    if(!store.state.auth.user || (store.state.auth.user.data && !store.state.auth.user.data.provider_id)) {
         redirect("/provider/login");
 
         store.commit("setIntendedUrl", route.fullPath);
